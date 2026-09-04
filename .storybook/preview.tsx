c1cee0779b2549afaa5fe7b70d1bfd10ef358572
Anna <annaing.dev@gmail.com>
2026-09-04 17:57:07 +0900

    feat: Storybook 매니저 다크모드와 컴포넌트 테마 동기화
    - storybook-dark-mode 설치, 매니저의 라이트/다크 버튼 하나로 컴포넌트 프리뷰(Canvas)와 Docs 페이지(배경/텍스트/표)까지 함께 전환되도록 연결

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,25 @@
 import type { Preview } from '@storybook/nextjs-vite'
+import { DocsContainer, type DocsContainerProps } from '@storybook/addon-docs/blocks'
+import { useEffect } from 'react'
+import { themes } from 'storybook/theming'
+import { useDarkMode } from 'storybook-dark-mode'
 import '../src/app/globals.css'
 import './pretendard.css'
+
+function ThemeSync({ children }: { children: React.ReactNode }) {
+  const isDark = useDarkMode();
+
+  useEffect(() => {
+    document.documentElement.dataset.theme = isDark ? 'dark' : 'light';
+  }, [isDark]);
+
+  return children;
+}
+
+function ThemedDocsContainer(props: DocsContainerProps) {
+  const isDark = useDarkMode();
+  return <DocsContainer {...props} theme={isDark ? themes.dark : themes.light} />;
+}
 
 const preview: Preview = {
   parameters: {
@@ -10,7 +29,17 @@ const preview: Preview = {
        date: /Date$/i,
       },
     },
+    backgrounds: {
+      disable: true,
+    },
+    darkMode: {
+      current: 'light',
+    },
+    docs: {
+      container: ThemedDocsContainer,
+    },
   },
+  decorators: [(Story) => <ThemeSync><Story /></ThemeSync>],
 };
 
 export default preview;
